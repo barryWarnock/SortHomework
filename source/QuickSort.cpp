034ad8d77b5b9e30cc60c78e5024ef10dfeffe16
barryWarnock <brrwarnock@gmail.com>
2015-12-03 14:15:57 -0800

    working quicksort

--- source/QuickSort.cpp
+++ source/QuickSort.cpp
@@ -8,46 +8,43 @@ int QuickSort::find_pivot(vector<int> &vectorToSort, int begining, int end) {
 	int middle = vectorToSort[begining + ((end - begining) / 2)];
 	int last = vectorToSort[end];
 
-	vector<int> possiblePivots= {first, middle, last};
-	SortParams params;
-	SortWrapper sortFascade;
-	sortFascade.sort(possiblePivots, params);
-	return possiblePivots[1];
+	if (first <= middle && first >= last || first >= middle && first <= last) {
+		return first;
+	}
+	if (middle <= first && middle >= last || middle >= first && middle <= last) {
+		return middle;
+	}
+	if (last <= middle && last >= first || last >= middle && last <= first) {
+		return last;
+	}
 }
 
 void QuickSort::sort(vector<int> &vectorToSort, SortParams params, int begining, int end) {
-	if (begining + 1 < end) {
+	if (begining < end) {
 		int pivot = find_pivot(vectorToSort, begining, end);
 
 		int i = begining;
 		int j = end;
-		bool partitioned = false;
-		while (!partitioned) {
+
+		while (i <= j) {
 
 			while (compare(pivot, vectorToSort[i], params.ascending)) {
 				i++;
-			}
+			} 
 
 			while (compare(vectorToSort[j], pivot, params.ascending)) {
 				j--;
-			}
+			} 
 
-			if (i < j) {
+			if (i <= j) {
 				std::swap(vectorToSort[i], vectorToSort[j]);
 				i++;
-				j++;
-			}
-			else {
-				partitioned = true;
+				j--;
 			}
 		}
 
-		if (j < end) {
-			sort(vectorToSort, params, begining, j);
-		}
-		if (j+1 > begining) {
-			sort(vectorToSort, params, j + 1, end);
-		}
+		sort(vectorToSort, params, begining, j);
+		sort(vectorToSort, params, i, end);
 	}
 	MemoryTracker::save_memory();
 }
